460901d0c4fb507724c667104d4740e66ab2b58e
Juan Carlos Rojas Garcia <juan.rojas@coppel.com>
2026-06-27 14:58:49 -0700

    feat: initial commit with spring boot 4.x, postgres and redis configuration

---
+++ src/main/java/com/ecommerce/ecommerce_api_catalog/EcommerceApiCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_api_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApiCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApiCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ecommerce_api_catalog/EcommerceApiCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_api_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
